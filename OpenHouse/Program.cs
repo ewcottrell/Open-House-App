9d50cf55b827bbf88da049a4625fe0c31de3a5ae
GRAYSON_LAPTOP\Grayson <grayson.ingram@att.net>
2018-08-17 12:56:53 -0500

    Little changes

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -11,8 +11,7 @@ namespace OpenHouse
             House House = new House();
             /*Go back and create classes for each of your rooms and make a 'create' method
             that creates each room and adds it and it's descriptions to the list of rooms.*/
-
-                     
+                        
             Realtor agent = new Realtor("Evan");
             agent.Greet();
             agent.RealtorScript();
